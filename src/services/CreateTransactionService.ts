e82142034f304f397e58ce7043428e8f4ce85768
Filipe Drummond <exgavian82@gmail.com>
2020-07-29 15:22:23 -0300

    :rocket: Finish all routes

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,52 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Saldo insuficiente', 400);
+    }
+
+    let categoryExist = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryExist) {
+      categoryExist = categoriesRepository.create({
+        title: category,
+      });
+      await categoriesRepository.save(categoryExist);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: categoryExist,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
